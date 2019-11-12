a0db9ff933b66aff8fd8778195372ce2093ac589
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-12 20:49:00 +0300

    update

--- test1.js
+++ test1.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
   if (error) {
-    console.error(error);
+    console.error(`Error${error}`);
   } else {
     console.log(`Content:${contents}`);
   }
