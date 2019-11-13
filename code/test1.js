057541f2565e6a21ecb7d6afa4fc47ef9dc31c83
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-13 14:34:12 +0300

    Remove code

--- code/test1.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
-  if (error) {
-    console.error(`Error:${error}!!1`);
-  } else {
-    console.log(`Content:${contents}!!1`);
-  }
-});
