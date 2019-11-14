4a34e60bb76e75763bb8bc9f82ead6b048e8751e
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-14 21:21:46 +0300

    Content update 7

--- test1.js
+++ test1.js
@@ -5,7 +5,7 @@ fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
     console.error(`Error:${error}!!`);
   } else {
     console.log(`Content:${contents}!`);
-    console.log('Content update: 6');
+    console.log('Content update: 7');
     console.log('Client update: 4');
     console.log('Code update: 1');
   }
