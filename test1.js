d59ed0038b57247b3f2498a6cb0eed3357366e4d
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-14 18:53:23 +0300

    Client update next

--- test1.js
+++ test1.js
@@ -7,5 +7,6 @@ fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
     console.log(`Content:${contents}!`);
     console.log('Very very special string from code!');
     console.log('Just to be sure (not) (maybe actually yes)');
+    console.log('MOAR');
   }
 });
