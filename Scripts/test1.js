98e2afcce1c7dfa936d35fbc6310f92282fe9941
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-14 23:12:45 +0300

    Content update 8

--- Scripts/test1.js
+++ Scripts/test1.js
@@ -5,7 +5,7 @@ fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
     console.error(`Error:${error}!!`);
   } else {
     console.log(`Content:${contents}!`);
-    console.log('Content update: 7');
+    console.log('Content update: 8');
     console.log('Client update: 4');
     console.log('Code update: 1');
   }
