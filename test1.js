2d081da5ec431c95ba28510348eb156d672d8405
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-16 12:22:35 +0300

    Squashed 'Scripts/' changes from a51be2f..411b64c
    411b64c Client update 8
    7249750 Content update 12
    
    git-subtree-dir: Scripts
    git-subtree-split: 411b64ccaa1d5ef8861e3fec79e374558e418ba6

--- test1.js
+++ test1.js
@@ -5,8 +5,8 @@ fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
     console.error(`Error:${error}!!`);
   } else {
     console.log(`Content:${contents}!`);
-    console.log('Content update: 11');
-    console.log('Client update: 7');
+    console.log('Content update: 12');
+    console.log('Client update: 8');
     console.log('Code update: 4');
   }
 });
