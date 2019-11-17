89faac95027ee83c09b128a582262253975b361a
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-17 18:58:01 +0300

    Squashed 'Scripts/' changes from 9ff27dc..7954a28
    7954a28 29
    
    git-subtree-dir: Scripts
    git-subtree-split: 7954a28460f6e159304b6241500a535d95b528aa

--- test1.js
+++ test1.js
@@ -5,7 +5,7 @@ fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
     console.error(`Error:${error}!!`);
   } else {
     console.log(`Content:${contents}!`);
-    console.log('Content update: 28');
+    console.log('Content update: 29');
     console.log('Client update: 20');
     console.log('Code update: 4');
   }
