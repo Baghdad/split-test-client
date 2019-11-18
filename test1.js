1c437aa1d7f57d490804a8e93cc98c5c3891fcde
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-18 20:57:54 +0300

    Squashed 'Scripts/' changes from c941a20..c8580df
    c8580df 38
    
    git-subtree-dir: Scripts
    git-subtree-split: c8580dfa5f63a3f43e2e5ccdecb9ac55eeb8f099

--- test1.js
+++ test1.js
@@ -5,7 +5,7 @@ fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
     console.error(`Error:${error}!!`);
   } else {
     console.log(`Content:${contents}!`);
-    console.log('Content update: 37');
+    console.log('Content update: 38');
     console.log('Client update: 21');
     console.log('Code update: 6');
   }
