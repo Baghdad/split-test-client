027dee728957e7586c8256e8f5e6142adef3297e
Kirill Bogdanov <baghdad232@gmail.com>
2019-11-18 20:47:12 +0300

    Squashed 'Scripts/' changes from ec86259..c941a20
    c941a20 Code 6
    
    git-subtree-dir: Scripts
    git-subtree-split: c941a20f9e3b9746c237ce781f7cfc110ff1fa91

--- test1.js
+++ test1.js
@@ -7,6 +7,6 @@ fs.readFile('./content/test1.txt', 'utf8', function(err, contents) {
     console.log(`Content:${contents}!`);
     console.log('Content update: 37');
     console.log('Client update: 21');
-    console.log('Code update: 5');
+    console.log('Code update: 6');
   }
 });
